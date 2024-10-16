3ed0b9113f200747c0b4bfdfa96a718d33f2b477
Ava Schmidt <avamschmidt07@gmail.com>
2024-10-16 07:31:12 -0500

    Initial Commit

---
+++ persistenceNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  persistenceNotes
+//
+//  Created by AVA SCHMIDT on 10/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
